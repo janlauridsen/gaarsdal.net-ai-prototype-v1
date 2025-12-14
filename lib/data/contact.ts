3caab9c0280836f76891521cb722f0ebeb1c84bf
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-14 22:11:39 +0100

    Update contact.ts

--- lib/data/contact.ts
+++ lib/data/contact.ts
@@ -6,7 +6,4 @@ Hjemmeside: https://gaarsdal.net
 E-mail: kontakt@gaarsdal.net
 Telefon: +45 42 80 74 74
 Lokation: Danmark
-
-Disse oplysninger er statiske og må kun bruges, når brugeren eksplicit efterspørger
-kontaktinformation, adresse eller praktiske oplysninger.
 `;
